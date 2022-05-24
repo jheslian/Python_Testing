e75a705e8134abf9bb2a953d8a87df6cc2fc8842
Jheslian <justinjhes@gmail.com>
2022-05-24 16:33:25 +0200

    locust performances

---
+++ perfomance_tests/locustfile.py
@@ -0,0 +1,33 @@
+from locust import HttpUser, task
+
+
+class ProjectPerfTest(HttpUser):
+    @task
+    def home(self):
+        self.client.get("/")
+
+    @task
+    def showSummary(self):
+        self.client.post("/showSummary", {"email": "john@simplylift.co"})
+
+    @task
+    def book(self):
+        self.client.get("/book/Spring%20Festival/Simply%20Lift")
+
+    @task
+    def purchasePlaces(self):
+        self.client.post("/purchasePlaces", {"competition": "Spring Festival",
+                                            "club": "Simply Lift",
+                                            "places": "1"})
+
+    @task
+    def points(self):
+        self.client.get("/points")
+
+    @task
+    def clubReservation(self):
+        self.client.get("/reservation/Spring%20Festival")
+
+    @task
+    def logout(self):
+        self.client.get("/logout")
