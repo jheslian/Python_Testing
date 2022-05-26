2e32d75f3ffd81a0d7e9114d541469e62cc43e71
Jheslian <justinjhes@gmail.com>
2022-05-26 08:46:04 +0200

    refactor code

--- server.py
+++ server.py
@@ -26,53 +26,62 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/showSummary', methods=['POST'])
+@app.route('/showSummary', methods=['GET', 'POST'])
 def showSummary():
-    clubs = loadClubs()
-    competitions = loadCompetitions()
-    for club in clubs:
-        club['totalPointsUsed'] = sum(club['noOfPlacesBookedOnCompetitions'].values())
     try:
-        session['email'] = request.form['email']
-        club = [club for club in clubs if club['email'] == request.form['email']]
-
+        if request.method == 'POST':
+            club = [club for club in clubs if club['email'] == request.form['email']][0]
+            session['email'] = club['email']
+        else:
+            club = [club for club in clubs if club['email'] == session['email']][0]
         if not club:
             return userFailedCredentialRedirection()
+
+        if 'noOfPlacesBookedOnCompetitions' in club:
+            club['totalPointsUsed'] = sum(club['noOfPlacesBookedOnCompetitions'].values())
+
     except KeyError:
         return userFailedCredentialRedirection()
-
-    return render_template('welcome.html', club=club[0], competitions=competitions)
+    return render_template('welcome.html', club=club, competitions=competitions)
 
 
 @app.route('/book/<competition>/<club>')
 def book(competition, club):
-    competitions = loadCompetitions()
-    clubs = loadClubs()
     if 'email' not in session:
         return userFailedCredentialRedirection()
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html', club=foundClub, competition=foundCompetition)
-    else:
+    try:
+        foundClub = [c for c in clubs if c['name'] == club][0]
+        foundCompetition = [c for c in competitions if c['name'] == competition][0]
+        if foundClub and foundCompetition:
+            # return redirect(f"/book/{club}/{competition}")
+
+            return render_template('booking.html', club=foundClub, competition=foundCompetition)
+    except:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return redirect(url_for('showSummary', data=session['email']))
+    return render_template('welcome.html', club=club, competitions=competitions)
 
 
 @app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
-    competitions = loadCompetitions()
-    clubs = loadClubs()
     if request.method != 'POST':
         return userFailedCredentialRedirection()
+
     MAX_PLACE_PER_CLUB = 12
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
     if request.method == 'POST' and placesRequired:
         tmpTotalPoints = 0
-        if competition['name'] in club['noOfPlacesBookedOnCompetitions']:
-            tmpTotalPoints = int(club['noOfPlacesBookedOnCompetitions'][competition['name']]) + placesRequired
+        if 'noOfPlacesBookedOnCompetitions' in club:
+
+            if competition['name'] in club['noOfPlacesBookedOnCompetitions']:
+                tmpTotalPoints = int(club['noOfPlacesBookedOnCompetitions'][competition['name']]) + placesRequired
+            # club['totalPointsUsed'] = sum(club['noOfPlacesBookedOnCompetitions'].values())
+        else:
+
+            tmpTotalPoints = placesRequired
+            # club['totalPointsUsed'] = placesRequired
         if int(competition['numberOfPlaces']) < placesRequired or int(club['points']) < placesRequired or \
                 tmpTotalPoints > MAX_PLACE_PER_CLUB or int(competition['numberOfPlaces']) == 0:
             if int(club['points']) < placesRequired:
@@ -85,39 +94,45 @@ def purchasePlaces():
                 flash("There's no more place left.")
 
             return render_template('booking.html', club=club, competition=competition)
+
+        """else:
+            
+            club['noOfPlacesBookedOnCompetitions'][competition['name']]= placesRequired"""
+        if 'noOfPlacesBookedOnCompetitions' in club:
+            if competition['name'] in club['noOfPlacesBookedOnCompetitions']:
+                club['noOfPlacesBookedOnCompetitions'][competition['name']] = int(
+                    club['noOfPlacesBookedOnCompetitions'][competition['name']]) + placesRequired
+            else:
+                club['noOfPlacesBookedOnCompetitions'][competition['name']] = placesRequired
         else:
-            updateNumberPlacesBookAndPointsByClubInJSONFile(club, competition, placesRequired)
-            # return redirect(url_for('book', club=club, competition=competition))
+            club['noOfPlacesBookedOnCompetitions'] = {}
+            club['noOfPlacesBookedOnCompetitions'][competition['name']] = placesRequired
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+        club['points'] = int(club['points']) - placesRequired
 
-    # competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
-    # club['points'] = int(club['points']) - placesRequired
-    # updateNumberOfPlacesAvailableInCompetitionJSONFile(competition, placesRequired)
+        flash('Great-booking complete!')
+        flash(
+            f"You have succesfully booked {placesRequired} place. The club has only {club['points']} points left.")
 
-    for club in clubs:
-        club['totalPointsUsed'] = sum(club['noOfPlacesBookedOnCompetitions'].values())
-
-    flash('Great-booking complete!')
-    flash(f"You have succesfully booked {placesRequired} place. The club has only {club['points']} points left.")
-
+        return redirect(url_for('showSummary'))
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
-@app.route('/points', methods=['GET'])
+@app.route('/points')
 def showClubPoints():
-    print(session)
-    clubs = loadClubs()
-    competitions = loadCompetitions()
     for club in clubs:
-        club['totalPointsUsed'] = sum(club['noOfPlacesBookedOnCompetitions'].values())
+        if 'noOfPlacesBookedOnCompetitions' in club:
+            club['totalPointsUsed'] = sum(club['noOfPlacesBookedOnCompetitions'].values())
+        else:
+            club['totalPointsUsed'] = 0
+
     return render_template('points.html', clubs=clubs, competitions=competitions)
 
 
 @app.route('/reservation/<competition>')
 def clubReservation(competition):
-    clubs = loadClubs()
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
-
     return render_template('reservation.html', clubs=clubs, competition=foundCompetition)
 
 
@@ -133,31 +148,3 @@ def userFailedCredentialRedirection():
     flash("Access denied. You're not log in or your email is not a valid.")
     return redirect(url_for('index'))
 
-
-def updateNumberPlacesBookAndPointsByClubInJSONFile(club, competition, noOfPlaces):
-    competitionName = competition['name']
-
-    with open("clubs.json", "r") as jsonFile:
-        data = json.load(jsonFile)
-
-        for i in data['clubs']:
-            if i['name'] == club['name']:
-                if competitionName in i['noOfPlacesBookedOnCompetitions']:
-                    i['noOfPlacesBookedOnCompetitions'][competitionName] += noOfPlaces
-                else:
-                    i['noOfPlacesBookedOnCompetitions'][competitionName] = noOfPlaces
-
-        with open("clubs.json", "w") as jsonFile:
-            json.dump(data, jsonFile)
-
-
-def updateNumberOfPlacesAvailableInCompetitionJSONFile(competition, noOfPlaces):
-    with open("competitions.json", "r") as jsonFile:
-        data = json.load(jsonFile)
-
-        for i in data['competitions']:
-            if i['name'] == competition['name']:
-                i['numberOfPlaces'] = str(int(i['numberOfPlaces']) - noOfPlaces)
-
-        with open("competitions.json", "w") as jsonFile:
-            json.dump(data, jsonFile)
